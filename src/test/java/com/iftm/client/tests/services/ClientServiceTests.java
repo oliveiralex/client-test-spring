1787a458b5b600b1510a983a9de25462f2b7ad83
Alexandre Oliveira <alexandre.si.ufu@gmail.com>
2021-06-10 22:27:10 -0300

    Comportamentos simulados mockito e testes no service

--- src/test/java/com/iftm/client/tests/services/ClientServiceTests.java
+++ src/test/java/com/iftm/client/tests/services/ClientServiceTests.java
@@ -1,20 +1,30 @@
 package com.iftm.client.tests.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.iftm.client.dto.ClientDTO;
+import com.iftm.client.entities.Client;
 import com.iftm.client.repositories.ClientRepository;
 import com.iftm.client.services.ClientService;
 import com.iftm.client.services.exceptions.DatabaseException;
 import com.iftm.client.services.exceptions.ResourceNotFoundException;
+import com.iftm.client.tests.factory.ClientFactory;
 
 @ExtendWith(SpringExtension.class)
 public class ClientServiceTests {
@@ -28,6 +38,10 @@ public class ClientServiceTests {
 	private long existingId;
 	private long nonExistingId;
 	private long dependentId;
+	private Client client;
+	private PageRequest pageRequest;
+	private PageImpl<Client> page;
+	private Double income;
 	
 	@BeforeEach
 	void setUp() throws Exception {
@@ -35,9 +49,26 @@ public class ClientServiceTests {
 		nonExistingId = 1000L;
 		dependentId = 4L;
 		
+		client = ClientFactory.createClient();
+		income = 4000.0;
+		pageRequest = PageRequest.of(0, 6);
+		page = new PageImpl<>(List.of(client));
+		
 		Mockito.doNothing().when(repository).deleteById(existingId);
 		Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
 		Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
+		
+		Mockito.when(repository.findAll(pageRequest))
+			.thenReturn(page);
+		
+		Mockito.when(repository.findByIncome(ArgumentMatchers.anyDouble(), ArgumentMatchers.any()))
+			.thenReturn(page);
+		
+		Mockito.when(repository.findById(existingId))
+			.thenReturn(Optional.of(client));
+		
+		Mockito.when(repository.findById(nonExistingId))
+			.thenReturn(Optional.empty());		
 	}
 	
 	@Test
@@ -70,6 +101,25 @@ public class ClientServiceTests {
 		Mockito.verify(repository, Mockito.times(1)).deleteById(dependentId);
 	}
 	
+	@Test
+	public void findAllPagedShouldReturnPage() {		
+		
+		Page<ClientDTO> result = service.findAllPaged(pageRequest);
+		
+		Assertions.assertNotNull(result);
+		Assertions.assertFalse(result.isEmpty());
+		
+		Mockito.verify(repository, Mockito.times(1)).findAll(pageRequest);
+	}
 	
+	@Test
+	public void findByIncomeShouldReturnPage() {
+		
+		Page<ClientDTO> result = service.findByIncome(income, pageRequest);
+		
+		Assertions.assertNotNull(result);
+		
+		Mockito.verify(repository, Mockito.times(1)).findByIncome(income, pageRequest);		
+	}	
 
 }
